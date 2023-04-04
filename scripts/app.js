f871ffb2f77fa603c7027a6c51ebef2a9eabe812
Afifa Ejaz <afifaejazz60@gmail.com>
2023-04-05 02:06:47 +0500

    First Program

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
-// alert('hello')
-// document.write("kaisay ho")
-// console.log('main theek')
+// Day 1 - 5 APRIL
+
+
+alert('hello')
+document.write("kaisay ho")
+console.log('main theek')
 
 
 
-// var name = 'afifa'
-// var fname = 'ejaz'
-// console.log(name +  fname)
+var name = 'afifa'
+var fname = 'ejaz'
+console.log(name +  fname)
 
 var name = prompt("what is your name?afifa")
-// document.write( "Assalam o alaikum" + " " + name)
+document.write( "Assalam o alaikum" + " " + name)
 console.log('assalamoalaikum' + namea)
